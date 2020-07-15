0234ee3970d07ac7894002bbed7d997ce906c15b
thonker <zheruel147@gmail.com>
2020-07-15 15:33:22 +0200

    Napisao bazu u sqlu, malo promjenio shemu baze sto posljedicno mijenja i sliku baze, kreirao ju na MS Sql Server-u i provjerio da sve radi kako treba

---
+++ Database resources/mydb.sql
@@ -0,0 +1,51 @@
+-- Create a new database called 'MrRobotWebshopDB'
+-- Connect to the 'master' database to run this snippet
+-- USE master
+-- GO
+-- Create the new database if it does not exist already
+-- IF NOT EXISTS (
+    -- SELECT name
+        -- FROM sys.databases
+        -- WHERE name = 'MrRobotWebshopDB'
+-- )
+-- CREATE DATABASE MrRobotWebshopDB
+-- GO
+
+CREATE TABLE Category (
+    CategoryID INT NOT NULL PRIMARY KEY,
+    CategoryName VARCHAR(255),
+);
+
+CREATE TABLE SubCategory (
+    SubCategoryID INT NOT NULL PRIMARY KEY,
+    SubCategoryName VARCHAR(255),
+    CategoryID INT FOREIGN KEY REFERENCES Category(CategoryID)
+);
+
+CREATE TABLE WebshopUser (
+    WebshopUserID INT NOT NULL PRIMARY KEY,
+    Username VARCHAR(255),
+    Password VARCHAR(255),
+    Firstname VARCHAR(255),
+    Lastname VARCHAR(255)
+);
+
+CREATE TABLE Receipt (
+    ReceiptID INT NOT NULL PRIMARY KEY,
+    Status VARCHAR(255),
+    FinalPrice VARCHAR(255)
+);
+
+CREATE TABLE Product (
+    ProductID INT NOT NULL PRIMARY KEY,
+    ProductName VARCHAR(255),
+    SubCategoryID INT FOREIGN KEY REFERENCES SubCategory(SubCategoryID)
+);
+
+CREATE TABLE ProductInfo (
+    ProductInfoID INT NOT NULL PRIMARY KEY,
+    Price DECIMAL,
+    Amount INT,
+    ProductID INT FOREIGN KEY REFERENCES Product(ProductID),
+    ReceiptID INT FOREIGN KEY REFERENCES Receipt(ReceiptID)
+);
